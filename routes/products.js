bfe691a1ecf7657692c87cc7985c8aaf85b5b7e3
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2019-12-19 00:11:22 +0200

    abdosoltan

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,21 @@
-
+const firebase = require("./../models/connect")
+    //var ref = firebase.database().ref().child('/1');
 const express = require('express');
 const router = express.Router();
 
 
 
 router.get('/products', (req, res, next) => {
+    var dbRef = firebase.database().ref('/');
+    dbRef.on("child_added", snap => {
+        products = snap.val();
+        console.log(products)
+        res.render('products', products)
+    });
 
 
-    res.render('products');
+
+
 
 })
 module.exports = router;
